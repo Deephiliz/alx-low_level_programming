f4b6b32c577fabafcedeee39519c06ac54a91219
Deephiliz <philipsdebs@gmail.com>
2023-04-26 06:43:01 -0700

    test-fix

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,4 @@
 #ifndef MAIN_H
 #define MAIN_H
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- * Return: On success 1.
- */
-int _putchar(char c)
+int _putchar(char c);
 #endif /* MAIN_H */
