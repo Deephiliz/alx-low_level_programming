70bfdd653349f8f1e9f2fb3bba3c95c3d52b9909
Deephiliz <philipsdebs@gmail.com>
2023-04-26 08:45:25 -0700

    fix

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -8,10 +8,15 @@
 int main(void)
 {
 int r;
-print_last_digit(98);
-print_last_digit(0);
-r = print_last_digit(-1024);
-_putchar('0' + r);
+r = print_last_digit(98);
+_putchar('\n');
+_putchar(r + '0');
+r = print_last_digit(0);
+_putchar('\n');
+_putchar(r + '0');
+r = print_last_digit(-98);
+_putchar('\n');
+_putchar(r + '0');
 _putchar('\n');
 return (0);
 }
