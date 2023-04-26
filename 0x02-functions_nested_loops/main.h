79bdee62fe27dcea4f9a209b926a1db36f016512
Deephiliz <philipsdebs@gmail.com>
2023-04-26 06:19:53 -0700

    fix

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,32 +2,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/** Main entry point
+ *
+ *
+ * */
+
 int _putchar(char c);
 
 #endif /* MAIN_H */
-
-
-/**int _putchar(void)
-void print_alphabet(void)
-
-void print_alphabet_x10(void)
-
-void _islower(int c)
-
-void _isalpha(int c)
-
-void print_sign(int n)
-
-void _abs(int)
-
-void print_last_digit(int)
-
-void jack_bauer(void)
-
-void times_table(void)
-
-void add(int, int)
-
-void print_to_98(int n)
-
-void print_times_table(int n) **/
