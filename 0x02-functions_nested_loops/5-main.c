36b560eba3aff781526d8a9af2ed560d9c23444b
Deephiliz <philipsdebs@gmail.com>
2023-04-26 08:07:24 -0700

    task-6

---
+++ 0x02-functions_nested_loops/5-main.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+i = 0;
+print_sign(i);
+_putchar('\n');
+i = 98;
+print_sign(i);
+_putchar('\n');
+i = 0;
+print_sign(i);
+_putchar('\n');
+i = 402;
+print_sign(i);
+_putchar('\n');
+i = -98;
+print_sign(i);
+_putchar('\n');
+return (0);
+}
