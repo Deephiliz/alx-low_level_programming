f18c3536a9ce6dfc556013f2490317405cd65685
Deephiliz <philipsdebs@gmail.com>
2023-04-26 10:03:01 -0700

    fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,45 +7,42 @@
  */
 void print_times_table(int n)
 {
-int i;
-int j;
-if (n <= 15 && n >= 0)
-{
+int i, j, result;
+if (n < 0 || n > 15)
+return;
 for (i = 0; i <= n; i++)
 {
 for (j = 0; j <= n; j++)
 {
-int prod = j * i;
+result = i * j;
 if (j == 0)
-{
-_putchar('0');
-}
-else if (prod <= 9)
+_putchar('0' + result);
+else if (result < 10)
 {
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(' ');
-_putchar('0' + prod);
+_putchar('0' + result);
 }
-else if (prod > 99)
+else if (result < 100)
 {
 _putchar(',');
 _putchar(' ');
-_putchar('0' + (prod / 100));
-_putchar('0' + ((prod / 10) % 10));
-_putchar('0' + (prod % 10));
+_putchar(' ');
+_putchar('0' + result / 10);
+_putchar('0' + result % 10);
 }
 else
 {
 _putchar(',');
 _putchar(' ');
-_putchar(' ');
-_putchar('0' + (prod / 10));
-_putchar('0' + (prod % 10));
+_putchar('0' + result / 100);
+_putchar('0' + (result / 10) % 10);
+_putchar('0' + result % 10);
 }
 }
 _putchar('\n');
 }
 }
-}
+
