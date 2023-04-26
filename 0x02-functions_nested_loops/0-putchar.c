73508f304a25d6e9cc0baf26cb3a27ce1e2164f7
Deephiliz <philipsdebs@gmail.com>
2023-04-26 06:51:54 -0700

    fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - 
+ * main -
  * Return: Always 0
  */
 
@@ -16,5 +16,5 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-return(0);
+return (0);
 }
