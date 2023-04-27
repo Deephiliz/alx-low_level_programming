a2e4341c6f85b8595a54b76f2f9f5d63bd75122f
Deephiliz <philipsdebs@gmail.com>
2023-04-27 15:36:41 -0700

    fix05

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,17 +6,14 @@
 
 void more_numbers(void)
 {
-int i;
-int h;
-for (h = 0; h <= 9; h++)
+int i, j;
+for (i = 0; i < 10; i++)
 {
-for (i = 0; i <= 14; i++)
+for (j = 0; j <= 14; j++)
 {
-if (i > 9)
-{
-_putchar(i / 10 + '0');
-}
-_putchar(i % 10 + '0');
+if (j >= 10)
+_putchar((j / 10) + '0');
+_putchar((j % 10) + '0');
 }
 _putchar('\n');
 }
