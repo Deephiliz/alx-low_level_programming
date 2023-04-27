37e09fc6bf6eb10431fdca0f4560bf9708a99b4c
Deephiliz <philipsdebs@gmail.com>
2023-04-27 14:34:32 -0700

    first-task

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ */
+int _isupper(int c);
+#endif /* MAIN_H */
