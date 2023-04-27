f171c9363e43bcdde0a9fb09b258ea9411bf73ab
Deephiliz <philipsdebs@gmail.com>
2023-04-27 15:17:01 -0700

    task-6

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints the numbers
+ *
+ */
+void more_numbers(void)
+{
+int i;
+for (i = '0'; i <= '9'; i++)
+{
+if (i != '2' &&  i != '4')
+{
+_putchar(i);
+}
+}
+_putchar('\n');
+}
