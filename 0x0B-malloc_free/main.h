f107972f341abff6fdbf65151a01f113cb12a56d
Deephiliz <philipsdebs@gmail.com>
2023-05-16 01:37:56 -0700

    task-2

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,4 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 char *create_array(unsigned int size, char c);
+/**
+ * _strdup - copies string
+ * @str: string to copy
+ * Return: character value
+ */
+char *_strdup(char *str);
 #endif
